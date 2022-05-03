21e93309766a248cb3b78adb771e888cbe509452
Md Masum Rana <masummx3@gmail.com>
2022-05-03 21:33:15 +0600

    first commit

---
+++ src/components/Home/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './Banner.css'
+
+const Banner = () => {
+  return (
+      <div className="banner">
+        <h2 className=" text-danger text-center"> this is my main Stock center</h2>
+      </div>
+  );
+};
+
+export default Banner;
