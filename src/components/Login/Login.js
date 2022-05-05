a70266467bd0c021b211de80ffaa6a81b756e157
Md Masum Rana <masummx3@gmail.com>
2022-05-05 18:22:06 +0600

    authentication social media

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,51 @@
 import React from "react";
 import { Button, Form } from "react-bootstrap";
-import { Link } from "react-router-dom";
+import { useSignInWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { Link, useNavigate } from "react-router-dom";
+import auth from "../../firebase.init";
+
 import './Login.css'
 import SocialMedia from "./SocialMedia/SocialMedia";
 
+
 const Login = () => {
+  const [
+    signInWithEmailAndPassword,
+    user,
+    loading,
+    error,
+  ] = useSignInWithEmailAndPassword(auth);
+  const navigate =useNavigate()
+
+const handleLogin=async(e)=>{
+  e.preventDefault();
+  const email=e.target.email.value;
+  const password=e.target.password.value;
+  console.log(email,password);
+  await signInWithEmailAndPassword(email,password);
+  navigate('/home')
+
+}
+
+const handleResetPass=()=>{
+  
+}
   return (
     <div className='w-50 mx-auto login-container'>
         <h3>Please Login</h3>
-      <Form className='form-container'>
+      <Form onSubmit={handleLogin} className='form-container'>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Control className='input-field' type="email" placeholder="Enter email" />
+          <Form.Control className='input-field' type="email" name='email' placeholder="Enter email" />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Control className='input-field' type="password" placeholder="Password" />
+          <Form.Control className='input-field' type="password" name='password' placeholder="Password" />
         </Form.Group>
-        <Button className='d-flex mx-auto fw-bold' variant="primary" type="submit">
+        <Button  className='d-flex mx-auto fw-bold' variant="primary" type="submit">
           Login
         </Button>
       </Form>
-      <p className='text-center'>Are you new? <Link to='/register'  className='text-danger text-decoration-none pe-auto'>Please Register</Link></p>
+      <p className='text-center'>Are you new? <Link  to='/register'  className='text-danger text-decoration-none pe-auto'>Please Register</Link></p>
+      <p className=' text-center'>Forget password? <button  onClick={handleResetPass} className='btn btn-link text-primary pe-auto'>Reset password</button></p>
       <SocialMedia></SocialMedia>
       
     </div>
--- src/components/Login/SocialMedia/SocialMedia.js
+++ src/components/Login/SocialMedia/SocialMedia.js
@@ -2,27 +2,49 @@ import React from "react";
 import google from "../../../Images/social/google.png"
 import github from "../../../Images/social/github.png"
 import facebook from "../../../Images/social/facebook.png"
+import { useSignInWithEmailAndPassword, useSignInWithFacebook, useSignInWithGithub, useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from "../../../firebase.init";
+import { useNavigate } from "react-router-dom";
 
 const SocialMedia = () => {
+  const [signInWithGoogle, userGoogle, loadingGoogle, errorGoogle] = useSignInWithGoogle(auth);
+  const [signInWithFacebook, userFacebook, loadingFacebook, errorFacebook] = useSignInWithFacebook(auth);
+  const [signInWithGithub, useGithub, loadingGithub, errorGithub] = useSignInWithGithub(auth);
+ 
+  const navigate =useNavigate()
+  let errorMessage
+  if(errorGoogle || errorFacebook ||errorGithub){
+    errorMessage = <div>
+      <p style={{color:'red'}}>Error: {errorGoogle?.message} {errorFacebook?.message}</p>
+    </div>
+  }
+
+ if(userGoogle || userFacebook ||useGithub){
+  navigate('/home')
+ }
   return (
     <div>
       <div className='d-flex align-items-center'>
         <div style={{height:'1px'}} className='bg-primary w-50'></div>
         <p className='mt-3 px-2'>Or</p>
         <div style={{height:'1px'}} className='bg-primary w-50'></div>
+        
       </div>
+      {
+          errorMessage
+        }
 
-      <button className='btn btn-info w-50 d-flex mx-auto justify-content-center align-items-center mb-2'>
+      <button onClick={() => signInWithGoogle()} className='btn btn-info w-50 d-flex mx-auto justify-content-center align-items-center mb-2'>
           <img src={google} alt="" />
           <p className="mx-2 mt-2">Google sign in</p>
       </button>
-      <button className='btn btn-info w-50 d-flex mx-auto justify-content-center align-items-center  mb-2'>
+      <button onClick={()=>signInWithGithub()} className='btn btn-info w-50 d-flex mx-auto justify-content-center align-items-center  mb-2'>
           <img src={github} alt="" />
-          <p className="mx-2 mt-2">Google sign in</p>
+          <p className="mx-2 mt-2">Github sign in</p>
       </button>
-      <button className='btn btn-info w-50 d-flex mx-auto justify-content-center align-items-center mb-2'>
+      <button onClick={()=>signInWithFacebook()} className='btn btn-info w-50 d-flex mx-auto justify-content-center align-items-center mb-2'>
           <img style={{width:'30px', height:'30px'}} src={facebook} alt="" />
-          <p className="mx-2 mt-2">Google sign in</p>
+          <p className="mx-2 mt-2">Facebook sign in</p>
       </button>
     </div>
   );
