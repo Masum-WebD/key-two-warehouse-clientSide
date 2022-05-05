f545201f47f193b9efee1c2c0dc0e93fa147f047
Md Masum Rana <masummx3@gmail.com>
2022-05-05 23:44:08 +0600

    add not found page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { Route, Routes } from 'react-router-dom';
 import Login from './components/Login/Login';
 import Header from './components/Home/Header/Header';
 import Register from './components/Login/Register/Register';
+import NotFound from './components/NotFound/NotFound';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
         <Route path="/home" element={<Home></Home>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
     </div>
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import img from '../../Images/404.png'
 
 const NotFound = () => {
     return (
         <div>
-            
+            <p className='d-flex justify-content-center align-items-center'>
+                <img src={img} alt="" />
+                <h3 > 404 not found page</h3>
+            </p>
         </div>
     );
 };
